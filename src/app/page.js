cb30012ad151d45e92e29c4a97ad7dd57dcbb0a6
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-09-06 22:07:03 +0600

    add: pure-super-shop project initialized

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Homepage from "@/components/Homepage/page";
+
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)] max-w-[1440px]">
+      <main>
+        <Homepage />
+      </main>
+    </div>
+  );
+}
---
+++ src/components/Homepage/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./Footer/page";
+import Navbar from "./Navbar/page";
+import Banner from "./Banner/page";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+      <Footer />
+    </div>
+  );
+};
+
+export default Homepage;
