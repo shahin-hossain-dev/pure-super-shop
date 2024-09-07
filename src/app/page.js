29c58cbdd17e78d163441df17452dbe1a16e6099
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-09-07 10:48:28 +0600

    added register api and some change file structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Homepage from "@/components/Homepage/page";
+import Banner from "@/components/Homepage/Banner";
 
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)] max-w-[1440px]">
       <main>
-        <Homepage />
+        <Banner></Banner>
       </main>
     </div>
   );
---
+++ src/app/register/api/route.js
@@ -0,0 +1,18 @@
+import { connectDB } from '@/lib/connectDB';
+export const POST = async (request) =>{
+const newUser = await request.json();
+try {
+    const db = await connectDB();
+    const userCollection = db.collection('users');
+    const isExist = await userCollection.findOne({email: newUser?.email})
+    console.log(newUser);
+    
+    if(isExist){
+        return Response.json({message: "user already exist"}, {status: 304})
+    }
+    const response = await userCollection.insertOne(newUser);
+    return Response.json({message: "user created"}, {status: 200})
+} catch (error) {
+    return Response.json({message: "something went wrong", error}, {status: 500})
+}
+}
