0d65545cbb143982dd6ca7c394b5eabce93464cc
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-09-07 16:00:03 +0600

    added banner section

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Banner from "@/components/Homepage/Banner";
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)] max-w-[1440px]">
+    <div className="font-[family-name:var(--font-geist-sans)] max-w-[1440px] w-[95%] md:w-11/12 mx-auto">
       <main>
         <Banner></Banner>
       </main>
--- src/app/register/api/route.js
+++ src/app/register/api/route.js
@@ -4,15 +4,14 @@ const newUser = await request.json();
 try {
     const db = await connectDB();
     const userCollection = db.collection('users');
-    const isExist = await userCollection.findOne({email: newUser?.email})
-    console.log(newUser);
+    const isExist = await userCollection.findOne({email: newUser?.email});
     
     if(isExist){
-        return Response.json({message: "user already exist"}, {status: 304})
+        return Response.json({message: "user already exist"}, {status: 304});
     }
     const response = await userCollection.insertOne(newUser);
-    return Response.json({message: "user created"}, {status: 200})
+    return Response.json({message: "user created"}, {status: 200});
 } catch (error) {
-    return Response.json({message: "something went wrong", error}, {status: 500})
+    return Response.json({message: "something went wrong", error}, {status: 500});
 }
 }
