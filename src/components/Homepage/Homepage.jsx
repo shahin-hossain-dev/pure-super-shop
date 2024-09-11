57fe7e646ccd0ec46e516b6622dc9962f59a3d37
tahfeezmizan <mizanahmed5316@gmail.com>
2024-09-11 22:30:17 +0600

    refector: home, newlatter, shopping side bar, footer, navbar

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -17,7 +17,6 @@ const Homepage = () => {
       <BigSell></BigSell>
       <Faq />
       <Newsletter />
-     
     </div>
   );
 };
--- src/components/sharePage/Navbar.jsx
+++ src/components/sharePage/Navbar.jsx
@@ -2,98 +2,98 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import { CiHeart } from "react-icons/ci"; 
+import { CiHeart } from "react-icons/ci";
 import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
-//   const [isOpen, setIsOpen] = useState(false);
-//   const session = useSession();
-//   console.log(session);
+  //   const [isOpen, setIsOpen] = useState(false);
+  //   const session = useSession();
+  //   console.log(session);
 
-//   return (
-//     <nav className="bg-white  sticky top-0 z-50">
-//       <div className="container mx-auto px-4">
-//         <div className="flex justify-between items-center py-6">
-//           {/* Logo */}
-//           <div className="text-2xl font-bold text-[#3e84b9]">
-//             <a href="#">
-//               Pure<span className="text-[#84b93e] font-bold">Super</span>
-//             </a>
-//           </div>
+  //   return (
+  //     <nav className="bg-white  sticky top-0 z-50">
+  //       <div className="container mx-auto px-4">
+  //         <div className="flex justify-between items-center py-6">
+  //           {/* Logo */}
+  //           <div className="text-2xl font-bold text-[#3e84b9]">
+  //             <a href="#">
+  //               Pure<span className="text-[#84b93e] font-bold">Super</span>
+  //             </a>
+  //           </div>
 
   const activeRoute = usePathname();
- 
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 
-  
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
   const navItems = [
     {
-        path: '/',
-        title: "Home"
+      path: '/',
+      title: "Home"
     },
     {
-        path: '/about',
-        title: "About"
+      path: '/about',
+      title: "About"
     },
     {
-        path: '/products',
-        title: "Products"
+      path: '/products',
+      title: "Products"
     },
-   
+
     {
-        path: '/contact',
-        title: "Contact"
+      path: '/contact',
+      title: "Contact"
     },
-]
-  if(activeRoute.includes('dashboard')) return 
+  ]
+  if (activeRoute.includes('dashboard')) return
 
 
-//           {/* Right-side buttons */}
-//           <div className="hidden md:flex items-center space-x-4">
-//             {session?.status === "unauthenticated" && (
-//               <Link
-//                 href="/checkout"
-//                 className="px-4 py-2 bg-[#3e84b9] text-white rounded hover:bg-blue-600"
-//               >
-//                 CheckOut
-//               </Link>
-//             )}
-//             {session?.status === "unauthenticated" && (
-//               <Link
-//                 href="/login"
-//                 className="px-4 py-2 bg-[#3e84b9] text-white rounded hover:bg-blue-600"
-//               >
-//                 Login
-//               </Link>
-//             )}
-//             {session?.status === "authenticated" && (
-//               <button
-//                 className="px-4 py-2 bg-[#ff1111] text-white rounded hover:bg-blue-600"
-//                 onClick={() => signOut()}
-//               >
-//                 Logout
-//               </button>
-//             )}
-//             {session?.status === "unauthenticated" && (
-//               <Link
-//                 href="/register"
-//                 className="px-4 py-2 bg-[#84b93e] text-white rounded hover:bg-green-600"
-//               >
-//                 Register
-//               </Link>
-//             )}
-//             <Link
-//               href="/wishlist"
-//               className="px-4 py-2 bg-white text-[#3e84b9] rounded  border font-medium border-[#3e84b9] flex items-center"
+  //           {/* Right-side buttons */}
+  //           <div className="hidden md:flex items-center space-x-4">
+  //             {session?.status === "unauthenticated" && (
+  //               <Link
+  //                 href="/checkout"
+  //                 className="px-4 py-2 bg-[#3e84b9] text-white rounded hover:bg-blue-600"
+  //               >
+  //                 CheckOut
+  //               </Link>
+  //             )}
+  //             {session?.status === "unauthenticated" && (
+  //               <Link
+  //                 href="/login"
+  //                 className="px-4 py-2 bg-[#3e84b9] text-white rounded hover:bg-blue-600"
+  //               >
+  //                 Login
+  //               </Link>
+  //             )}
+  //             {session?.status === "authenticated" && (
+  //               <button
+  //                 className="px-4 py-2 bg-[#ff1111] text-white rounded hover:bg-blue-600"
+  //                 onClick={() => signOut()}
+  //               >
+  //                 Logout
+  //               </button>
+  //             )}
+  //             {session?.status === "unauthenticated" && (
+  //               <Link
+  //                 href="/register"
+  //                 className="px-4 py-2 bg-[#84b93e] text-white rounded hover:bg-green-600"
+  //               >
+  //                 Register
+  //               </Link>
+  //             )}
+  //             <Link
+  //               href="/wishlist"
+  //               className="px-4 py-2 bg-white text-[#3e84b9] rounded  border font-medium border-[#3e84b9] flex items-center"
 
   return (
     <nav className="flex border-b py-4 max-w-[1440px] w-[95%] md:w-11/12 mx-auto font-[sans-serif] min-h-[80px] tracking-wide relative z-50">
       <div className="flex flex-wrap items-center lg:gap-y-2 gap-4 w-full">
-        <Link className="text-2xl font-bold text-[#3e84b9]" href="/"> 
+        <Link className="text-2xl font-bold text-[#3e84b9]" href="/">
           Pure<span className="text-[#84b93e] font-bold">Super</span>
         </Link>
         <div
@@ -118,8 +118,8 @@ const Navbar = () => {
           <ul className="lg:flex lg:gap-x-3 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50 text-center">
             {/* Mobile Menu Items */}
             {
-        navItems?.map((item) => <li key="title"><Link  className={item?.path === activeRoute ? "font-bold pb-2 !text-[#000] react-tabs__tab " : "react-tabs__tab"} href={item?.path}>{item?.title}</Link></li>)
-      }
+              navItems?.map((item) => <li key="title"><Link className={item?.path === activeRoute ? "font-bold pb-2 !text-[#000] react-tabs__tab " : "react-tabs__tab"} href={item?.path}>{item?.title}</Link></li>)
+            }
             {/* Add menu items here */}
             {/* Mobile specific links */}
             <li><Link href="/login" className="px-4 lg:hidden py-2 bg-[#3e84b9] text-white rounded hover:bg-blue-600 block text-center">Login</Link></li>
