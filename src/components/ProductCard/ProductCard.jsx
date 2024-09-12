4cd2effd11538ed8caf6d867484522f81749a05a
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-09-12 13:38:00 +0600

    update: product card

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -36,17 +36,6 @@ const ProductCard = (product) => {
     console.log(id);
   };
   return (
- {/* View and Wishlist Buttons */}
-    {/*<div className="card relative flex flex-col justify-between border hover:border-[#84b93e] hover:border-2 rounded-sm p-2">
-     
-      <div className="right-3 top-3 absolute flex flex-col view-wish">
-        <button
-          onClick={() => handleDetails("id")}
-          className="bg-gray-100 mb-3 p-2 text-[#333333] rounded-full"
-        >
-          <FaRegEye className="text-2xl " />
-        </button>*/}
-
     <div className="card box-border relative flex flex-col justify-between border hover:border-[#84b93e] duration-300 hover:border-1 rounded-sm p-2">
       <div className=" right-3 top-3 absolute flex flex-col view-wish">
         <Link href={`products/${_id}`}>
@@ -76,30 +65,20 @@ const ProductCard = (product) => {
       </div>
 
       {/* Product Info and Fixed Buttons */}
-      <div className="flex flex-col justify-end flex-grow">
+      <div className="flex flex-col justify-end flex-grow mt-2">
         <h3 className="text-xl mb-2 text-center">{productName}</h3>
         <p className="text-xl mb-2 text-center text-[#84b93e] font-medium">
           ${price}
         </p>
 
-
         {/* Add to Cart Button Fixed at the Bottom */}
-        <div className="mt-auto">
-          <button
-            onClick={handleAddToCart}
-            className="w-full p-2 bg-gradient-to-t from-gray-300 to-gray-50"
-          >
-            Add to Cart
-          </button>
-        </div>
-        {/* <button
+        <div className="mt-auto"></div>
+        <button
           onClick={handleAddToCart}
           className="w-full mt-4 bottom-2 left-2 rounded-sm active:scale-95 right-2 p-1 bg-gray-300 hover:bg-[#84b93e] duration-300  active:bg-[#6a9630] hover:text-white"
         >
           Add to Cart
-        </button> */}
-        
-
+        </button>
       </div>
     </div>
   );
